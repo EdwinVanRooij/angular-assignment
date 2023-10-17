d1258b34442911b7b8319def3c25c5e91e22c6fa
Edwin van Rooij <edwinvrooij@gmail.com>
2023-10-17 11:13:11 +0200

    setup for placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,42 +10,17 @@ import { StoreKeys } from './state/store-keys';
 import { clientReducer } from './state/client/client.reducers';
 import { ClientEffects } from './state/client/client.effects';
 import { NotFoundComponent } from './not-found/not-found.component';
-
-// I'm just importing this here for now manually, but ideally you'd have a
-// shared module with these kinds of imports so that you can reuse them in other
-// modules.
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
   imports: [
     BrowserModule,
+    MaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     StoreModule.forRoot({ [StoreKeys.Client]: clientReducer }),
     EffectsModule.forRoot([ClientEffects]),
-    MatSnackBarModule,
-    MatDialogModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    MatCardModule,
-    MatDividerModule,
-    MatToolbarModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -3,42 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './details/details.component';
 import { OverviewComponent } from './overview/overview.component';
 import { CreateComponent } from './create/create.component';
-
-// I'm just importing this here for now manually, but ideally you'd have a
-// shared module with these kinds of imports so that you can reuse them in other
-// modules.
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MaterialModule } from '../shared/material/material.module';
+import { UtilitiesModule } from '../shared/utilities/utilities.module';
 
 @NgModule({
   declarations: [DetailsComponent, OverviewComponent, CreateComponent],
-  imports: [
-    CommonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    MatCardModule,
-    MatDividerModule,
-    MatToolbarModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MatProgressSpinnerModule,
-  ],
+  imports: [CommonModule, MaterialModule, UtilitiesModule],
 })
 export class ClientsModule {}
