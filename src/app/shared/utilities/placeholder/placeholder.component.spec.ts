f1d1478b1e434f6548e91b1e187001f754d34f41
Edwin van Rooij <edwinvrooij@gmail.com>
2023-11-06 19:25:30 +0100

    add placeholder tests

--- src/app/shared/utilities/placeholder/placeholder.component.spec.ts
+++ src/app/shared/utilities/placeholder/placeholder.component.spec.ts
@@ -1,21 +1,42 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlaceholderComponent } from './placeholder.component';
+import { MatIconModule } from '@angular/material/icon';
 
-// import { PlaceholderComponent } from './placeholder.component';
+describe('PlaceholderComponent', () => {
+  let component: PlaceholderComponent;
+  let fixture: ComponentFixture<PlaceholderComponent>;
+  let element: HTMLElement;
 
-// describe('PlaceholderComponent', () => {
-//   let component: PlaceholderComponent;
-//   let fixture: ComponentFixture<PlaceholderComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MatIconModule],
+      declarations: [PlaceholderComponent],
+    }).compileComponents();
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [PlaceholderComponent]
-//     });
-//     fixture = TestBed.createComponent(PlaceholderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(PlaceholderComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the message', () => {
+    const testMessage = 'Test Message';
+    component.message = testMessage;
+    fixture.detectChanges();
+
+    const messageElement = element.querySelector('h2');
+    expect(messageElement?.textContent).toBe(testMessage);
+  });
+
+  it('should display the correct icon', () => {
+    const testIcon = 'test_icon';
+    component.iconKey = testIcon;
+    fixture.detectChanges();
+
+    const iconElement = element.querySelector('mat-icon');
+    expect(iconElement?.textContent).toBe(testIcon);
+  });
+});
