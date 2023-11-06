2b6b97771adff9116fe76b6c2a153163e9a9426b
Edwin van Rooij <edwinvrooij@gmail.com>
2023-11-06 19:36:19 +0100

    fix tests

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,21 +1,39 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NotFoundComponent } from './not-found.component';
+import { By } from '@angular/platform-browser';
+import { PlaceholderComponent } from '../shared/utilities/placeholder/placeholder.component';
 
-// import { NotFoundComponent } from './not-found.component';
+describe('NotFoundComponent', () => {
+  let component: NotFoundComponent;
+  let fixture: ComponentFixture<NotFoundComponent>;
 
-// describe('NotFoundComponent', () => {
-//   let component: NotFoundComponent;
-//   let fixture: ComponentFixture<NotFoundComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NotFoundComponent, PlaceholderComponent],
+    }).compileComponents();
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [NotFoundComponent]
-//     });
-//     fixture = TestBed.createComponent(NotFoundComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should pass correct message to app-placeholder', () => {
+    const placeholderEl = fixture.debugElement.query(
+      By.directive(PlaceholderComponent)
+    );
+    expect(placeholderEl.componentInstance.message).toEqual('Not found');
+  });
+
+  it('should pass correct iconKey to app-placeholder', () => {
+    const placeholderEl = fixture.debugElement.query(
+      By.directive(PlaceholderComponent)
+    );
+    expect(placeholderEl.componentInstance.iconKey).toEqual('warning');
+  });
+});
