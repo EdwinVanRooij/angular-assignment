dd65ced3eeba1e0f2b34cd606b2dca9f1d6f46c3
Edwin van Rooij <edwinvrooij@gmail.com>
2023-11-06 18:03:02 +0100

    fix app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,43 @@
-// import { TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { MaterialModule } from './shared/material/material.module';
 
-// describe('AppComponent', () => {
-//   beforeEach(() => TestBed.configureTestingModule({
-//     imports: [RouterTestingModule],
-//     declarations: [AppComponent]
-//   }));
+describe('AppComponent', () => {
+  let store: MockStore;
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+  const initialState = {};
 
-//   it(`should have as title 'AngularAssignment'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('AngularAssignment');
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, StoreModule.forRoot({}), MaterialModule],
+      declarations: [AppComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement as HTMLElement;
-//     expect(compiled.querySelector('.content span')?.textContent).toContain('AngularAssignment app is running!');
-//   });
-// });
+    store = TestBed.inject(MockStore);
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'AngularAssignment'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('AngularAssignment');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('mat-card-subtitle')?.textContent).toContain(
+      'Use this application to view and add clients.'
+    );
+  });
+});
--- src/app/persons/overview/person-overview.component.spec.ts
+++ src/app/persons/overview/person-overview.component.spec.ts
@@ -1,21 +1,49 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { OverviewComponent } from './overview.component';
+import { Person } from 'src/app/models/person';
 
-// import { OverviewComponent } from './overview.component';
+describe('OverviewComponent', () => {
+  let component: OverviewComponent;
+  let fixture: ComponentFixture<OverviewComponent>;
+  let store: Store;
 
-// describe('OverviewComponent', () => {
-//   let component: OverviewComponent;
-//   let fixture: ComponentFixture<OverviewComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [OverviewComponent],
+      imports: [NgxsModule.forRoot([])],
+      providers: [Store],
+    }).compileComponents();
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [OverviewComponent]
-//     });
-//     fixture = TestBed.createComponent(OverviewComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(OverviewComponent);
+    component = fixture.componentInstance;
+    store = TestBed.inject(Store);
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render persons from the store', () => {
+    const mockPersons: Person[] = [
+      { id: 0, firstName: 'John', lastName: 'Doe' },
+      { id: 1, firstName: 'Jane', lastName: 'Doe' },
+    ];
+    store.reset({ persons: { persons: mockPersons } });
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelectorAll('div').length).toBe(2);
+    expect(compiled.querySelector('div').textContent).toContain('John Doe');
+  });
+
+  it('should handle no persons gracefully', () => {
+    store.reset({ persons: { persons: [] } });
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelectorAll('div').length).toBe(0);
+  });
+});
