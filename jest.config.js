59dbe1c9f3db3b57bac42734286fdfc33b773c0b
Edwin van Rooij <edwinvrooij@gmail.com>
2023-11-06 17:19:51 +0100

    idk

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,8 @@ globalThis.ngJest = {
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
   globalSetup: "jest-preset-angular/global-setup",
   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: "<rootDir>" }),
-  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
 };
--- src/app/clients/overview/overview.component.spec.ts
+++ src/app/clients/overview/overview.component.spec.ts
@@ -1,15 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OverviewComponent } from './overview.component';
+
+// describe('OverviewComponent', () => {
+//   let component: OverviewComponent;
+//   let fixture: ComponentFixture<OverviewComponent>;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [OverviewComponent]
+//     });
+//     fixture = TestBed.createComponent(OverviewComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [OverviewComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OverviewComponent],
+    }).compileComponents(); // Use compileComponents when working with external templates or styles
+
     fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
