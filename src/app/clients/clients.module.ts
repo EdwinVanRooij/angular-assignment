57a9863591747cae68fa065109555c3e34a22207
Edwin van Rooij <edwinvrooij@gmail.com>
2023-11-18 14:22:41 +0100

    querying clients by querylist

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -5,9 +5,10 @@ import { OverviewComponent } from './overview/overview.component';
 import { CreateComponent } from './create/create.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { UtilitiesModule } from '../shared/utilities/utilities.module';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
-  declarations: [DetailsComponent, OverviewComponent, CreateComponent],
+  declarations: [DetailsComponent, OverviewComponent, CreateComponent, ClientComponent],
   imports: [CommonModule, MaterialModule, UtilitiesModule],
 })
 export class ClientsModule {}
--- src/app/clients/overview/overview.component.ts
+++ src/app/clients/overview/overview.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, QueryList, ViewChildren} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
+import {filter, Observable, Subscription, tap} from 'rxjs';
 import { Client } from 'src/app/models/client';
 import { ClientState } from 'src/app/state/client/client.reducers';
 import {
@@ -9,21 +9,43 @@ import {
   selectClients,
 } from 'src/app/state/client/client.selectors';
 import { CreateComponent } from '../create/create.component';
+import {ClientComponent} from "../client/client.component";
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
 })
-export class OverviewComponent {
+export class OverviewComponent implements AfterViewInit, OnDestroy {
+  @ViewChildren(ClientComponent)
+  clientComponents!: QueryList<ClientComponent>;
+
   client$: Observable<Client[]>;
   selectedClient: Client | null = null;
   isLoadingClients$: Observable<boolean>;
-  subscriptions: Subscription[] = [];
+  subscriptions: Subscription[] = []
 
   constructor(private store: Store<ClientState>, private dialog: MatDialog) {
-    this.isLoadingClients$ = this.store.select(isLoadingClients);
+    this.isLoadingClients$ = this.store.select(isLoadingClients).pipe();
     this.client$ = this.store.select(selectClients);
+  }
+
+  ngAfterViewInit(): void {
+    const clientSubscription = this.clientComponents.changes
+      .pipe(filter((change) => change!! && change.length > 0)).subscribe(() => this.onClientComponentsRendered());
+
+    this.subscriptions.push(clientSubscription);
+  }
+
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe()
+    }
+  }
+
+  onClientComponentsRendered(): void {
+    console.log(`Found all ClientComponent elements by querying the template ` +
+      `using the component's type with the decorator @ViewChildren: ${this.clientComponents.length}`)
   }
 
   onSelectClient(client: Client): void {
