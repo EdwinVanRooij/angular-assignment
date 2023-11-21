15a31a51ea72bf3b434b0e82280e8826282fa709
Edwin van Rooij <edwinvrooij@gmail.com>
2023-11-21 18:22:25 +0100

    add ngx unless directive

---
+++ src/app/shared/directives/ngx-unless.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+	selector: '[appNgxUnless]',
+})
+export class NgxUnlessDirective {
+	constructor(
+		private templateRef: TemplateRef<any>,
+		private viewContainer: ViewContainerRef
+	) {
+		// viewContainer.
+	}
+}
--- src/app/shared/utilities/utilities.module.ts
+++ src/app/shared/utilities/utilities.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { MaterialModule } from '../material/material.module';
 import { HighlightedDirective } from '../directives/highlighted.directive';
+import { NgxUnlessDirective } from '../directives/ngx-unless.directive';
 
 @NgModule({
-	declarations: [PlaceholderComponent, HighlightedDirective],
+	declarations: [PlaceholderComponent, HighlightedDirective, NgxUnlessDirective],
 	imports: [CommonModule, MaterialModule],
-	exports: [PlaceholderComponent, HighlightedDirective],
+	exports: [PlaceholderComponent, HighlightedDirective, NgxUnlessDirective],
 })
 export class UtilitiesModule {}
